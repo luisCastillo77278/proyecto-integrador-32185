73f96bd1b7a996b42afd27a4fc4728bb80dfabee
luis jesus villegas castillo <lc77278@gmail.com>
2022-12-24 21:23:26 -0600

    added config prod mongo

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+//! import firebase
+//! import enviroment
+import { enviroment } from '../enviroments/env.js'
+
+export const firebaseConnection = () =>{
+  const { connection } = enviroment.databases.firebase;
+
+}
--- src/enviroments/env.js
+++ src/enviroments/env.js
@@ -1,7 +1,9 @@
 export const enviroment = {
  databases: {
     mongo: {
-      url: process.env.NODE === "dev" ? "mongodb://lc77278:lc77278@localhost:27017" : "",
+      url: process.env.NODE === "dev" 
+        ? "mongodb://lc77278:lc77278@localhost:27017" 
+        : "mongodb+srv://lc77278:lc77278@cluster0.vkqhkh4.mongodb.net",
       nameDB:  "coderhouse"
     },
     firebase: {
